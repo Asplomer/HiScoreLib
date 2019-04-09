b8b5f003f96f57be9f316704635685b261a00328
Ariel <asplomer@gmail.com>
2019-04-09 11:56:51 -0300

    v0.9 added most functions fixed bugs

--- HiScore/HiScoreLib/Hi.cpp
+++ HiScore/HiScoreLib/Hi.cpp
@@ -1,1 +1,59 @@
 #include "Hi.h"
+Hi::Hi()
+{
+
+	hScore = 0;
+}
+
+
+void Hi::addChar(string cName, int cScore, int num)
+{
+	lis[num].name = cName;
+	lis[num].score = cScore;
+	updateHScore();
+
+}
+void Hi::addScore(int num,int toAdd)
+{
+	lis[num].score += toAdd;
+	updateHScore();
+}
+int Hi::getScore(int num)
+{
+	return lis[num].score;
+}
+void Hi::setScore(int num, int scor)
+{
+	if (lis[num].name != "") 
+	{
+		lis[num].score = scor;
+		updateHScore();
+	}
+}
+void Hi::showScore()
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		if(lis[i].name !="")
+		cout << "Character:" << lis[i].name.c_str() << "   " << "score:" << lis[i].score << endl;
+	}
+}
+void Hi::showHScore()
+{
+	cout << hScore<< endl;
+}
+int Hi::getHScore()
+{
+	return hScore;
+}
+void Hi::updateHScore()
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (lis[i].score > hScore) {
+			if (lis[i].name != "")
+			hScore = lis[i].score;
+		}
+	}
+}
+
--- HiScore/HiScoreLib/Hi.h
+++ HiScore/HiScoreLib/Hi.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <iostream>
 using namespace std;
-struct character {
+#define SIZE 5
+struct character 
+{
 	string name;
 	int score;
 };
@@ -9,12 +11,17 @@ struct character {
 class Hi
 {
 private:
-	character lis[5];
+	character lis[SIZE];
+	int hScore;
 
 public:
-	void character(string cName, int cScore);
-	void addChar(string cName);
-	void addScore(int toAdd);
-	int getScore(string cName);
+	Hi();
+	void addChar(string cName, int cScore, int num);
+	void addScore(int num,int toAdd);
+	int getScore(int num);
+	void setScore(int num, int scor);
 	void showScore();
+	void showHScore();
+	int getHScore();
+	void updateHScore();
 };
